3c1312373ca7e99f183509f459831d2cab083c7e
andrey_moseev <[wer_ander@mail.ru]>
2023-01-17 02:09:25 +0300

    Улучшил Модель Статьи и Доделал основные страницы приложения до минимального функционала

--- Kurs/Models/Article.cs
+++ Kurs/Models/Article.cs
@@ -6,11 +6,18 @@ namespace Kurs.Models
     public class Article
     {
         public int Id { get; set; }
+        [RegularExpression(@"^[А-Я]+[а-яА-я\s]+[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         [Display(Name = "Название" )]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Title { get; set; }
         [Display(Name = "Категория")]
+        [RegularExpression(@"^[А-Я]+[а-яА-я\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Categoru { get; set; }
-        
+        [Display(Name = "Текст")]
+        [RegularExpression(@"^[А-Я]+[а-яА-я\s]+[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? Text { get; set; }
 
     }
