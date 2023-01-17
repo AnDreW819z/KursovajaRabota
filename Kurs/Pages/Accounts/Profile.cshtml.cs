fe3b93a33fe53d84ede654c94124d47e4a4bcc40
andrey_moseev <[wer_ander@mail.ru]>
2023-01-17 18:07:26 +0300

    Отредактировал страницу лк и прописал логику изменения аккаунта

--- Kurs/Pages/Accounts/Profile.cshtml.cs
+++ Kurs/Pages/Accounts/Profile.cshtml.cs
@@ -1,12 +1,39 @@
+using Kurs.Data;
+using Kurs.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kurs.Pages.Accounts
 {
     public class ProfileModel : PageModel
     {
+        [BindProperty]
+        public Account account { get; set; }
+        private KursContext db;
+        public ProfileModel(KursContext _db)
+        {
+            db = _db;
+        }
         public void OnGet()
         {
+            var username = HttpContext.Session.GetString("username");
+            account = db.Accounts.SingleOrDefault(a => a.UserName.Equals(username));
+        }
+        public IActionResult OnPost() 
+        {
+            if (!string.IsNullOrEmpty(account.Password))
+            {
+                account.Password = BCrypt.Net.BCrypt.HashPassword(account.Password);
+            }
+            else
+            {
+                account.Password = db.Accounts.AsNoTracking().SingleOrDefault(a => a.Id == account.Id).Password;
+            }
+            db.SaveChanges();
+            db.Entry(account).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToPage("../Index");
         }
     }
 }
